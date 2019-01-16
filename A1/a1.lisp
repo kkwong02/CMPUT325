2f16dbf86510b807f21ac9bda33d42cffad1d3ba
Kirsten Kwong <kskwong@ualberta.ca>
2019-01-16 12:10:56 -0700

    added provided tests and added a helper function for pallindromes

--- A1/a1.lisp
+++ A1/a1.lisp
@@ -8,7 +8,7 @@
 (defun len (L)
     (if (null L) 
         0 
-        (+  (len (rest L)) 1) 
+        (+  (len (cdr L)) 1) 
     )
 )
 
@@ -19,26 +19,45 @@
 (defun issorted (L)
     (if (OR (= 0 (len L)) (= 1 (len L)) )
         T
-        (if (< (first L) (first(rest L)))
-            (issorted (rest L))
+        (if (< (car L) (cadr L)))
+            (issorted (cdr L))
             NIL
         )
     )
 )
 
-
-;QUESTION 2 numbers
-; (numbers N)
-; this function produces a list of integers from 1 up to N, given a non negative integer.
+; QUESTION 2 numbers
+; (numbers (N))
+; this function produces a list of integers from 1 up to N, given a non negative
+; integer. the function will return NIL if the number given is less than 1.
 (defun numbers (N)
     (if (< N 1)
         NIL
         
         (if (= N 1)
-            (list 1)
-            (append (numbers (- N 1)) (list N))
+            (list 1) 
+            (cons (numbers (- N 1)) N)
         )
     )
 )
 
-;QUESTION 3 palindrome
+; QUESTION 3 palindrome
+; (splice (L))
+; A helper function for splicing a list, L.
+; Removes the first and last elements of a list. Will return NIL if less than 3 elements.
+(defun splice (L)
+    (cond
+        ((< (len L) 3) NIL)
+        ((= (len L) 3) (list (cadr L)))
+        (T (cons (cadr L) (splice (cdr L))))
+    )
+)
+
+; (getLast L)
+; A helper function that gets the last element of a list
+(defun (getLast L)
+)
+
+; (palindrome (L) )
+; This function checks if a list of characters is a palindrome by recursively checking the 
+; first and last characters.
